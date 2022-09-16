74b28e11f3c55a2beab7f3c24d2ee5db0e8215b7
mohammad <m.mobasher.z@gmail.com>
2022-09-17 02:22:47 +0430

    config well database

--- database/database.go
+++ database/database.go
@@ -2,6 +2,8 @@ package database
 
 import (
 	"context"
+	"fmt"
+	"log"
 
 	"github.com/MohammadMobasher/resturan-backend/models"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -15,11 +17,17 @@ func ConnectDB(ctx context.Context, config models.Configuration) *mongo.Database
 	cred.Username = config.MongoUsername
 	cred.Password = config.MongoPassword
 
-	clientOption := options.Client().ApplyURI(config.MongoServer)
+	clientOption := options.Client().ApplyURI(config.MongoServer).SetAuth(cred)
 	client, err := mongo.Connect(ctx, clientOption)
 	if err != nil {
 		panic(err)
 	}
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		fmt.Println("Ping has problem")
+		log.Fatal(err)
+	}
+
 	return client.Database(config.MongoDatabase)
 
 }
