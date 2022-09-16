f41d9ac83e187e959837323c5e5a8ea478123ed4
mohammad <m.mobasher.z@gmail.com>
2022-09-16 15:29:37 +0430

    create config func to load from env

---
+++ config/config.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"github.com/MohammadMobasher/resturan-backend/models"
+	"github.com/spf13/viper"
+)
+
+func GetConfig() models.Configuration {
+	config := models.Configuration{}
+
+	viper.SetConfigName("")    // name of config file (without extension)
+	viper.SetConfigType("env") // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath("./")  // path to look for the config file in
+	viper.AutomaticEnv()
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		panic(err)
+	}
+
+	err = viper.Unmarshal(&config)
+	if err != nil { // Handle errors reading the config file
+		panic(err)
+
+	}
+
+	return config
+}
