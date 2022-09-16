84358168e15710e740e981cb96e6d259af95315b
mohammad <m.mobasher.z@gmail.com>
2022-09-17 02:22:26 +0430

    init user model

---
+++ models/user.go
@@ -0,0 +1,9 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+	Id       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name     string             `json:"name,omitempty"`
+	UserName string             `json:"username,omitempty"`
+}
