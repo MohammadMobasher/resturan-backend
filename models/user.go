13743082f21c9681e9072e02e1f8245c44d49685
mohammad <m.mobasher.z@gmail.com>
2022-09-18 00:28:29 +0430

    add requierd annotation to user

--- models/user.go
+++ models/user.go
@@ -4,7 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
 	Id       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Name     string             `json:"name,omitempty"`
-	UserName string             `json:"username,omitempty"`
-	Password string             `json:"password,omitempty" bson:"password,omitempty"`
+	Name     string             `json:"name,omitempty" bson:"name,omitempty" binding:"required"`
+	UserName string             `json:"username,omitempty" binding:"required"`
+	Password string             `json:"password,omitempty" binding:"required"`
 }
