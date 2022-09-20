74002f3a2580be843675a3b6710757adadb7f96d
mohammad <m.mobasher.z@gmail.com>
2022-09-20 17:05:38 +0430

    create route

---
+++ routes/foodgroup_route.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/MohammadMobasher/resturan-backend/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func FoodGroupRoute(r *gin.Engine) {
+	r.GET("/foodgroup", controllers.GetFoodGroups)
+	// r.POST("/foodgroup", controllers.CreateFoodGroup)
+	// r.PUT("/foodgroup", controllers.UpdateFoodGroup)
+	// r.GET("/foodgroup/:foodgroupId", controllers.GetFoodGroup)
+	// r.DELETE("/foodgroup/:foodgroupId", controllers.DeleteFoodGroup)
+}
