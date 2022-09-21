f79b5c90af327a3196d163ba9ca78dc8caa70d75
mohammad <m.mobasher.z@gmail.com>
2022-09-21 13:44:21 +0430

    add upload_file folder to gitignore

---
+++ docs/docs.go
@@ -0,0 +1,25 @@
+// Package classification Resturan.
+//
+// Documentation of our Resturan API.
+//
+//     Schemes: http
+//     BasePath: /
+//     Version: 1.0.0
+//     Host: some-url.com
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+//     Security:
+//     - basic
+//
+//    SecurityDefinitions:
+//    basic:
+//      type: basic
+//
+// swagger:meta
+
+package docs
