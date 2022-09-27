a8420092a514b87d8027643187cf7888170d1c4c
mohammad <m.mobasher.z@gmail.com>
2022-09-27 12:58:37 +0330

    create function for General uploading

---
+++ controllers/utility.go
@@ -0,0 +1,33 @@
+package controllers
+
+import (
+	"net/http"
+	"path/filepath"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+func uploadFile(c *gin.Context) (string, error) {
+	file, err := c.FormFile("file")
+
+	// The file cannot be received.
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"message": "No file is received",
+		})
+		return "", err
+	}
+
+	extension := filepath.Ext(file.Filename)
+	newFileName := uuid.New().String() + extension
+
+	if err := c.SaveUploadedFile(file, "uploaded_file/"+newFileName); err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"message": err,
+		})
+		return "", err
+	}
+
+	return newFileName, nil
+}
