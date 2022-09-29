b98a4de03accf9cc8105bb2f1d1f98b316f7e5cb
mohammad <m.mobasher.z@gmail.com>
2022-09-29 20:40:03 +0330

    change file name database to database_mongo

---
+++ database/database_mongo.go
@@ -0,0 +1,33 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/MohammadMobasher/resturan-backend/models"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectMongoDB(ctx context.Context, config models.Configuration) *mongo.Database {
+
+	var cred options.Credential
+
+	cred.Username = config.MongoUsername
+	cred.Password = config.MongoPassword
+
+	clientOption := options.Client().ApplyURI(config.MongoServer).SetAuth(cred)
+	client, err := mongo.Connect(ctx, clientOption)
+	if err != nil {
+		panic(err)
+	}
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		fmt.Println("Ping has problem")
+		log.Fatal(err)
+	}
+
+	return client.Database(config.MongoDatabase)
+
+}
