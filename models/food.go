32f6759071bbb5221a27613874e62a12a0bf05da
mohammad <m.mobasher.z@gmail.com>
2022-10-13 16:44:14 +0330

    create food entity for mysql

--- models/food.go
+++ models/food.go
@@ -8,3 +8,10 @@ type Food struct {
 	FoodGroupId  string             `json:"FoodGroupId"  bson:"FoodGroupId,omitempty" binding:"required"`
 	ImageAddress string
 }
+
+type FoodMySql struct {
+	Id           int64  `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name         string `json:"name,omitempty" bson:"name,omitempty" binding:"required"`
+	FoodGroupId  int64  `json:"FoodGroupId"  bson:"FoodGroupId,omitempty" binding:"required"`
+	ImageAddress *string
+}
