1091d34f32e909de7411afb483e9d71c7f64ea7c
mohammad <m.mobasher.z@gmail.com>
2022-10-13 16:43:52 +0330

    new function for create food v2 routes

--- routes/food_route.go
+++ routes/food_route.go
@@ -2,12 +2,22 @@ package routes
 
 import (
 	controllersv1 "github.com/MohammadMobasher/resturan-backend/controllers/v1"
+	controllersv2 "github.com/MohammadMobasher/resturan-backend/controllers/v2"
+
 	"github.com/gin-gonic/gin"
 )
 
 func FoodRouteV1(r *gin.RouterGroup) {
 	// r.GET("/foodgroup", controllers.GetFoodGroups)
 	r.POST("/food", controllersv1.CreateFood)
+	// r.PUT("/foodgroup", controllers.UpdateFoodGroup)
+	// r.GET("/foodgroup/:foodgroupId", controllers.GetFoodGroup)
+	// r.DELETE("/foodgroup/:foodgroupId", controllers.DeleteFoodGroup)
+}
+
+func FoodRouteV2(r *gin.RouterGroup) {
+	// r.GET("/foodgroup", controllers.GetFoodGroups)
+	r.POST("/food", controllersv2.CreateFood)
 	// r.PUT("/foodgroup", controllers.UpdateFoodGroup)
 	// r.GET("/foodgroup/:foodgroupId", controllers.GetFoodGroup)
 	// r.DELETE("/foodgroup/:foodgroupId", controllers.DeleteFoodGroup)
