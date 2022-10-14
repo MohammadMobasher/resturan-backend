f9abb1c70f8610c418855ce6e1f9a7869b43ead7
mohammad <m.mobasher.z@gmail.com>
2022-10-14 14:49:57 +0330

    create new route for user v2

--- routes/user_route.go
+++ routes/user_route.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	controllersv1 "github.com/MohammadMobasher/resturan-backend/controllers/v1"
+	controllersv2 "github.com/MohammadMobasher/resturan-backend/controllers/v2"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,4 +12,12 @@ func UserRouteV1(r *gin.RouterGroup) {
 	r.PUT("/user", controllersv1.UpdateUsers)
 	r.GET("/user/:userId", controllersv1.GetUser)
 	r.DELETE("/user/:userId", controllersv1.DeleteUser)
+}
+
+func UserRouteV2(r *gin.RouterGroup) {
+	r.GET("/users", controllersv2.GetUsers)
+	r.POST("/user", controllersv2.CreateUser)
+	r.PUT("/user", controllersv2.UpdateUsers)
+	r.GET("/user/:userId", controllersv2.GetUser)
+	r.DELETE("/user/:userId", controllersv2.DeleteUser)
 }
