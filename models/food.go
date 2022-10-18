f07bf57c6a0a991072f80777cb3b79f08452f53a
mohammad <m.mobasher.z@gmail.com>
2022-10-18 23:43:03 +0330

    create table for saving food image

--- models/food.go
+++ models/food.go
@@ -9,9 +9,23 @@ type Food struct {
 	ImageAddress string
 }
 
+type FoodImage struct {
+	Id           primitive.ObjectID `json:"id,omitempty" bson:"id,omitempty"`
+	FoodId       primitive.ObjectID `json:"FoodGroupId"  bson:"FoodGroupId,omitempty" binding:"required"`
+	ImageAddress string             `binding:"required"`
+}
+
+///////////////////////////////////////////////////////////////////////
+
 type FoodMySql struct {
 	Id           int64  `json:"id,omitempty" bson:"id,omitempty"`
 	Name         string `json:"name,omitempty" bson:"name,omitempty" binding:"required"`
 	FoodGroupId  int64  `json:"FoodGroupId"  bson:"FoodGroupId,omitempty" binding:"required"`
 	ImageAddress *string
+}
+
+type FoodImageMySql struct {
+	Id           int64   `json:"id,omitempty" bson:"id,omitempty"`
+	FoodId       int64   `json:"FoodGroupId"  bson:"FoodGroupId,omitempty" binding:"required"`
+	ImageAddress *string `binding:"required"`
 }
