7bed5d606d56f136ba2a578ff0f6e95dd0c5d8b1
mohammad <m.mobasher.z@gmail.com>
2022-10-18 23:44:09 +0330

    handle multiple img and save in the deseire table

--- repositories/mysql_repository/food_mysql_repository.go
+++ repositories/mysql_repository/food_mysql_repository.go
@@ -30,7 +30,7 @@ func NewFoodMySqlRepository() *FoodMySqlRepository {
 	}
 }
 
-func (f *FoodMySqlRepository) Insert(food models.FoodMySql) (*models.FoodMySql, error) {
+func (f *FoodMySqlRepository) Insert(food models.FoodMySql, images []string) (*models.FoodMySql, error) {
 	q := "INSERT INTO food(Name, FoodGroupId, ImageAddress) VALUES(?, ?, ?)"
 	insert, err := f.db.Prepare(q)
 	if err != nil {
@@ -54,6 +54,37 @@ func (f *FoodMySqlRepository) Insert(food models.FoodMySql) (*models.FoodMySql, 
 	}
 
 	food.Id = lastInsertId
+
+	if len(images) > 0 {
+
+		q_images := "INSERT INTO food_image(FoodId, ImageAddress) VALUES "
+		vals := []interface{}{}
+
+		for _, image := range images {
+			q_images += "(?, ?),"
+			vals = append(vals, food.Id, image)
+		}
+
+		log.Println(q_images)
+
+		q_images = q_images[0 : len(q_images)-1]
+		//prepare the statement
+		insertImage, err := f.db.Prepare(q_images)
+
+		if err != nil {
+			log.Println(err)
+			f.Delete(lastInsertId)
+			return nil, err
+		}
+
+		//format all vals at once
+		_, err = insertImage.Exec(vals...)
+		if err != nil {
+			log.Println(err)
+			f.Delete(lastInsertId)
+			return nil, err
+		}
+	}
 
 	return &food, nil
 }
