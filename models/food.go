98681cbc6f1b9a00219240ae08e9b0b879b222c7
mohammad <m.mobasher.z@gmail.com>
2022-10-22 15:31:39 +0330

    add Images prop to FoodMySqlDTO

--- models/food.go
+++ models/food.go
@@ -1,6 +1,8 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Food struct {
 	Id           primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
@@ -35,5 +37,6 @@ type FoodMySqlDTO struct {
 	Name        string         `json:"name,omitempty" bson:"name,omitempty" binding:"required"`
 	FoodGroupId int64          `json:"foodgroupid"  bson:"foodgroupid,omitempty" binding:"required"`
 	Image       *string        `json:"image"  bson:"image"`
+	Images      []string       `json:"images"  bson:"images"`
 	FoodGroup   FoodGroupMySql `json:"foodgroup" bson:"foodgroup"`
 }
