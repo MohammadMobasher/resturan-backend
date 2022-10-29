5deee060c488f62e009686288b2b11c9cae45028
mohammad <m.mobasher.z@gmail.com>
2022-10-29 17:40:44 +0330

    create food_comment model

---
+++ models/food_comment.go
@@ -0,0 +1,7 @@
+package models
+
+type FoodCommentMySql struct {
+	Id      int64  `json:"id,omitempty" bson:"id,omitempty"`
+	FoodId  int64  `json:"foodid,omitempty" bson:"foodid,omitempty" binding:"required"`
+	Comment string `json:"comment" bson:"comment"`
+}
